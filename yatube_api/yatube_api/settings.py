22a2900d4e09ecc46cbbf1a8ff0da42ae56b6c41
Локтев Андрей <lordham@yandex.ru>
2023-03-20 03:33:39 +0300

    удалил django_filters

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -18,8 +18,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
-    'django_filters',
     'djoser',
     'api',
     'posts',
