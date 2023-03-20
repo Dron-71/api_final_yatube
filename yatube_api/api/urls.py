6046118b186fb81fc241b53494fcb04d0e1b9f28
Локтев Андрей <lordham@yandex.ru>
2023-03-20 03:05:49 +0300

    api

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -9,6 +9,7 @@ router.register('follow', FollowViewSet, basename='follow')
 router.register(r'posts/(?P<post_id>\d+)/comments',
                 CommentViewSet, basename='comments')
 
+
 urlpatterns = [
     path('v1/', include(router.urls)),
     path('v1/', include('djoser.urls')),
